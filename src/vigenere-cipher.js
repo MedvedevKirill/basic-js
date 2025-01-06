68fca383a7c9d1e621336c8e3d029ddaf3d09393
Medvedev Kirill <medvedev_k@icloud.com>
2025-01-06 16:24:42 +0500

    feat: implement VigenereCipheringMachine

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,61 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+    if (!this.isDirect) {
+      this._addToResult = (newChar, result) => {
+        return newChar + result;
+      } 
+    }
+    else {
+      this._addToResult = (newChar, result) => {
+        return result + newChar;
+      } 
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  _ALPHABET = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  encrypt(str, key) {
+    if (typeof str !== 'string' || typeof key !== 'string') {
+      throw new Error('Incorrect arguments!');
+    }
+    let result = '';
+    for (let i = 0, j = 0; i < str.length; i += 1, j += 1) {
+      const currElementAlphabetIndex = this._ALPHABET.indexOf(str[i].toLowerCase());
+      if (currElementAlphabetIndex === -1) {
+        result = this._addToResult(str[i], result);
+        j -= 1;
+      }
+      else {
+        const currKeyElementAlphabetIndex = this._ALPHABET.indexOf(key[j % key.length].toLowerCase());
+        const newChar = this._ALPHABET[(currElementAlphabetIndex + currKeyElementAlphabetIndex) % this._ALPHABET.length].toUpperCase();
+        result = this._addToResult(newChar, result);
+      }
+    }
+    return result;
+  }
+  decrypt(str, key) {
+    if (typeof str !== 'string' || typeof key !== 'string') {
+      throw new Error('Incorrect arguments!');
+    }
+    let result = '';
+    for (let i = 0, j = 0; i < str.length; i += 1, j += 1) {
+      const currElementAlphabetIndex = this._ALPHABET.indexOf(str[i].toLowerCase());
+      if (currElementAlphabetIndex === -1) {
+        result = this._addToResult(str[i], result);
+        j -= 1;
+      }
+      else {
+        const currKeyElementAlphabetIndex = this._ALPHABET.indexOf(key[j % key.length].toLowerCase());
+        let newCharIndex = currElementAlphabetIndex - currKeyElementAlphabetIndex;
+        if (newCharIndex < 0) {
+          newCharIndex += this._ALPHABET.length;
+        }
+        const newChar = this._ALPHABET[newCharIndex].toUpperCase();
+        result = this._addToResult(newChar, result);
+      }
+    }
+    return result;
   }
 }
 
